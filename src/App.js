348e522480800ffe2eb79f3a74cdd0a3195e03af
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-09 18:51:43 -0600

    working on structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import Logo from "./assets/components/logo";
+import MenuIcon from "./assets/components/menuIcon";
+
 function App() {
-  return <></>;
+  const [showNavigation, setShowNavigation] = useState(false);
+  return (
+    <>
+      <nav className="navigation">
+        <div className="container">
+          <Logo />
+
+          {showNavigation ? (
+            <div className="menu">
+              <ul>
+                <li></li>
+              </ul>
+            </div>
+          ) : (
+            <MenuIcon />
+          )}
+        </div>
+      </nav>
+      <main className="main"></main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/components/menuIcon.js
@@ -0,0 +1,12 @@
+const MenuIcon = () => {
+  return (
+    <svg viewBox="0 0 40 17" xmlns="http://www.w3.org/2000/svg">
+      <g fill="#00001A" fillRule="evenodd">
+        <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
+        <path d="M0 0h40v3H0z" />
+      </g>
+    </svg>
+  );
+};
+
+export default MenuIcon;
