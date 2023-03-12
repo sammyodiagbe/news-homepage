3fa03804888449ab26b9bd46f03b5d513e7d2b3c
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-12 10:26:29 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -36,45 +36,47 @@ function App() {
         </div>
       </nav>
       <main className="main">
-        <section className="">
-          <aside className="left-main-content">
-            <div className="bottom">
-              <h1>The Bright Future of Web 3.0?</h1>
-              <div className="writeup">
-                <p>
-                  We dive into the next evolution of the web that claims to put
-                  the power of the platforms back into the hands of the people.
-                  But is it really fulfilling its promise?
-                </p>
-                <a href="#read-more" className="btn">
-                  Read More
-                </a>
+        <div className="container">
+          <section className="">
+            <aside className="left-main-content">
+              <div className="bottom">
+                <h1>The Bright Future of Web 3.0?</h1>
+                <div className="writeup">
+                  <p>
+                    We dive into the next evolution of the web that claims to
+                    put the power of the platforms back into the hands of the
+                    people. But is it really fulfilling its promise?
+                  </p>
+                  <a href="#read-more" className="btn">
+                    Read More
+                  </a>
+                </div>
               </div>
-            </div>
-          </aside>
-          <aside className="right-aside-new">
-            <h1>New</h1>
-            <div className="a-news">
-              <h3>Hydrogen vs Electric Cars</h3>
-              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-            </div>
-            <div className="a-news">
-              <h3>The DownSides of AI Artistry</h3>
-              <p>
-                What are the possible adverse effects of on-demand AI image
-                generation?
-              </p>
-            </div>
-            <div className="a-news">
-              <h3>is VC Funding Drying Up?</h3>
-              <p>
-                Private funding by VC firms is down 50% YOY. We take a look at
-                what that means.
-              </p>
-            </div>
-          </aside>
-        </section>
-        <section className="top-news"></section>
+            </aside>
+            <aside className="right-aside-new">
+              <h1>New</h1>
+              <div className="a-news">
+                <h3>Hydrogen vs Electric Cars</h3>
+                <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+              </div>
+              <div className="a-news">
+                <h3>The DownSides of AI Artistry</h3>
+                <p>
+                  What are the possible adverse effects of on-demand AI image
+                  generation?
+                </p>
+              </div>
+              <div className="a-news">
+                <h3>is VC Funding Drying Up?</h3>
+                <p>
+                  Private funding by VC firms is down 50% YOY. We take a look at
+                  what that means.
+                </p>
+              </div>
+            </aside>
+          </section>
+          <section className="top-news"></section>
+        </div>
       </main>
     </>
   );
